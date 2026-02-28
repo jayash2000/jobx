df36ea69976313abcecafbca59ee2772bf26527c
Jayash Basnet <jayashbasnetit@gmail.com>
2026-02-28 21:09:54 +0545

    Initial Nuxt setup

---
+++ app/app.vue
@@ -0,0 +1,31 @@
+<script setup>
+useHead({
+  meta: [{ name: "viewport", content: "width=device-width, initial-scale=1" }],
+  link: [{ rel: "icon", href: "/favico.svg" }],
+  htmlAttrs: {
+    lang: "en",
+  },
+});
+
+const title = "JobX";
+const description =
+  "Connect with top employers, explore thousands of job listings, and take the next step in your career. Sign in to continue or create an account to get started.";
+
+useSeoMeta({
+  title,
+  description,
+  ogTitle: title,
+  ogDescription: description,
+  ogImage: "https://www.github.com/nature.png",
+  // twitterImage: "https://ui.nuxt.com/assets/templates/nuxt/starter-light.png",
+  // twitterCard: "summary_large_image",
+});
+</script>
+
+<template>
+  <UApp>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </UApp>
+</template>
